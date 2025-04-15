ec2211b2d84752ee30afee0968c68cf7f78f394f
minahk <minah2815@naver.com>
2025-04-15 14:16:05 +0900

    변수 입력&출력

---
+++ src/study/var/variable06.java
@@ -0,0 +1,33 @@
+package study.var;
+
+import java.util.Scanner;
+
+public class variable06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// 사용자 입력
+		
+		Scanner scanner = new Scanner(System.in); //Scanner 생성 
+		
+		System.out.println("입력");
+		
+		String inputData = scanner.nextLine(); // 다음 엔터까지 입력된 값 읽어오기 
+		
+		System.out.println("입력완료");
+		System.out.println(inputData);
+		
+		System.out.print("지금 몇시죠? : ");
+		int hour = scanner.nextInt();
+		
+		System.out.printf("아 ~ 지금 %d시 이군요!\n", hour);
+		
+		System.out.print("중간고사 평균 점수 입력 : ");
+		double mean = scanner.nextDouble();
+		System.out.printf("아 %.2f점 이군요 ~ \n" , mean);
+	}
+	
+
+}
